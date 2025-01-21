29afa23b0f91df5a68791d179996b36b8c09ef6b
Beforeandafterc <gfdee6366eoq@outlook.com>
2025-01-21 20:58:43 +0100

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
-export const PUMP_AMOUNT_USD = 0.01;
+export const PUMP_AMOUNT_USD = 2.00;
 export const REFERRAL_PERCENTAGE = 50; // 50% split
 export const RECIPIENT_WALLET = "8vTmuQ6H8ASDcQsnFNYgYopzfedgY4iFBChHKD2kEmU1";
