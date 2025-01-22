0742a85e3bf7416bd06ae67c267f7468f716318e
Beforeandafterc <gfdee6366eoq@outlook.com>
2025-01-22 20:10:57 +0100

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 export const PUMP_AMOUNT_USD = 3.00;
 export const REFERRAL_PERCENTAGE = 50; // 50% split
-export const RECIPIENT_WALLET = "8vTmuQ6H8ASDcQsnFNYgYopzfedgY4iFBChHKD2kEmU1";
+export const RECIPIENT_WALLET = "5qR5tjHF2Eq64vwpHwFfzLQm1tiJsL7UwoafaZdQKYK5";
